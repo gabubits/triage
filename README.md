297478af5f742c7e82b3fb89abd8a75e6c800d07
gabriel gomes <114315570+gabubits@users.noreply.github.com>
2025-09-29 15:30:14 -0300

    feat: triage initial files

---
+++ README.md
@@ -0,0 +1,40 @@
+# Triage (Projeto desenvolvido para o Case Prático da empresa AutoU)
+
+O **Triage** é um projeto feito como solução para o desafio proposto como Case Prático da empresa AutoU.
+
+## Descrição breve do desafio
+
+O objetivo central do case é **automatizar a leitura** e **sugerir classificações e respostas automáticas** de acordo com o teor de cada email recebido.
+
+Existem duas categorias de classificação:
+
+- Emails **Produtivos**: Emails que requerem uma ação ou resposta específica (ex.: solicitações de suporte técnico, atualização sobre casos em aberto, dúvidas sobre o sistema); e
+
+- Emails **Improdutivos**: Emails que não necessitam de uma ação imediata (ex.: mensagens de felicitações, agradecimentos).
+
+Para elevar a experiência de uso do usuário e do modelo, também foi desenvolvido uma **interface** com o **Gradio**, framework essencial para construção visual de modelos de machine learning.
+
+Em síntese, o desafio é composto de três principais etapas:
+
+1. Treinamento do modelo especializado em classificação de e-mails em produtivo/improdutivo em Python;
+2. Criação da interface para o usuário classificar seus e-mails; e
+3. Deploy da aplicação na nuvem.
+
+## Solução
+
+Para atigir esses objetivos, proponho o **Triage**, um sistema especialmente criado para esse case prático com foco na classificação de emails e resposta automática. Em resumo, o modelo principal do Triage foi treinado através de um
+processo de _fine-tuning_ no modelo popularmente conhecido [DistilBERT base multilingual](https://huggingface.co/distilbert/distilbert-base-multilingual-cased) para especializa-lo na tarefa principal de classíficação de e-mails.
+
+Para verificar o modelo treinado, ele foi exportado no formato Hugging Face Transformes e está disponível aqui: [https://huggingface.co/gabubits/triage-portuguese](https://huggingface.co/gabubits/triage-portuguese)
+
+Para testar a aplicação, ela está deployada e hospedada no Hugging Face Spaces: [https://huggingface.co/spaces/gabubits/triage-demo-email-classifier](https://huggingface.co/spaces/gabubits/triage-demo-email-classifier).
+
+## Arquivos principais
+
+- [`dataset.csv`](dataset.csv) - Arquivo com a especificação do dataset usado para treinamento. Esse detaset é um conjunto de 391 e-mails classificados entre produtivo e improdutivo. Foi gerado de forma sintética e orientada com o apoio do Gemini.
+- [`triage_model_training_process.ipynb`](triage_model_training_process.ipynb) - Arquivo com a especificação e detalhamento de todo o processo de treinamento do modelo DistilBERT base multilingual para especializa-lo em ser um classificador de e-mails.
+- [triage_frontend.py](triage_frontend.py) - Arquivo de interface baseada em Gradio. Nesse arquivo é especificado o uso do modelo diretamente do Hugging Faces Transformers, o processo de predição de um novo e-mail e o design da interface, focado na simplicidade e nas funcionalidades.
+
+## Pastas principais
+
+- [triage_model](triage_model) - Arquivos resultantes do treinamento e que foram upados no Hugging Face Transformers.
---
+++ triage_frontend.py
@@ -0,0 +1,131 @@
+# Triage - Gradio Interface Config
+
+# Importação das bibliotecas principais
+import gradio as gr                     # Criação da interface web
+import torch                            # Inferência do modelo em PyTorch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import numpy as np
+import pymupdf                          # Leitura de arquivos PDF
+
+# Carrega modelo e tokenizer do Hugging Face
+# Esse modelo foi treinado realizando um fine-tuning no modelo
+# DistilBERT base multilingual (cased), para especializa-lo
+# na classificação de emails.
+model_name = "gabubits/triage-portuguese"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+# Labels do classificador
+label_names = ["Improdutivo", "Produtivo"]
+
+# Função de predição principal
+def predict_email(assunto, conteudo, arquivo):
+
+    # Evita que usuário insira texto e arquivo ao mesmo tempo
+    if (assunto.strip() or conteudo.strip()) and arquivo is not None:
+      return "## ❌ Erro: Por favor, faça somente o upload do email ou a descrição do e-mail"
+
+    # Caso seja enviado arquivo .txt ou .pdf
+    if arquivo is not None:
+        if arquivo.name.endswith(".txt"):
+            with open(arquivo.name, "r", encoding="utf-8") as f:
+              conteudo = f.read()
+        if arquivo.name.endswith(".pdf"):
+            conteudo = "" 
+            doc = pymupdf.open(arquivo.name)
+            for page in doc:
+              conteudo += page.get_text()
+
+    # Exige que pelo menos o corpo do email seja enviado
+    if not conteudo.strip():
+        return "## ❌ Erro: Por favor, digite, pelo menos, o corpo do email"
+    
+    # Monta entrada para o modelo
+    input_text = f"SUBJECT: {assunto} BODY: {conteudo}"
+    inputs = tokenizer(input_text, return_tensors="pt", truncation=True, padding=True, max_length=256)
+    
+    # Predição com PyTorch sem gradiente
+    with torch.no_grad():
+        outputs = model(**inputs)
+    
+    # Probabilidades normalizadas
+    probs = torch.nn.functional.softmax(outputs.logits, dim=1).squeeze().numpy()
+    predicted_class = np.argmax(probs)
+    
+    # Emojis para facilitar visualização
+    emoji_map = {
+        "Produtivo": "✅",
+        "Improdutivo": "❌"
+    }
+    
+    # Montagem da resposta formatada
+    result_label = label_names[predicted_class]
+    confidence = float(probs[predicted_class])
+    emoji = emoji_map.get(result_label, "")
+    result = f"## Resultado: {emoji} {result_label.upper()} ({confidence*100:.1f}%)\n\n"
+
+    # Inclui assunto e conteúdo recebidos
+    if assunto.strip():
+        result += f"- **Assunto recebido:** {assunto}\n\n"
+    else:
+        result += f"- **Assunto recebido:** Não foi enviado um assunto.\n\n"
+    result += f'- **Conteúdo recebido:** \n\n"{conteudo}"\n\n'
+
+    # Sugestões automáticas de resposta
+    responses = {
+        "Produtivo": "Obrigado pelo contato. Ficamos felizes em receber sua mensagem.",
+        "Improdutivo": "Olá! Sua solicitação foi recebida e será analisada em breve. Retornaremos com novidades."
+    }
+
+    result += f"### Sugestão de resposta:\n{responses[result_label]}\n\n"
+    
+    # Tabela com distribuição das probabilidades
+    result += "### Distribuição de probabilidade\n"
+    result += "|Categoria|Probabilidade|\n|-|-|\n"
+
+    sorted_probs = [(label, float(probs[i])) for i, label in enumerate(label_names)]
+    sorted_probs.sort(key=lambda x: x[1], reverse=True)
+
+    for label, prob in sorted_probs:
+        result += f"|{label}|{prob*100:.1f}%|\n"
+        
+    return result
+
+# Construção da interface com Gradio
+with gr.Blocks() as demo:
+    gr.Markdown("# Triage - Classificador de e-mails")
+    gr.Markdown("### Olá, esse é o Triage: um classificador de e-mails produtivos e improdutivos. O modelo principal foi refinado (fine-tuning) com base no modelo DistilBERT base multilingual (cased) e é especializado na classificação (produtivo ou improdutivo) de e-mails ou arquivos.")
+
+    with gr.Row():
+        # Coluna esquerda: inputs
+        with gr.Column(scale=1):
+            assunto = gr.Textbox(label="Assunto do e-mail", placeholder="Digite o assunto do e-mail")
+            conteudo = gr.Textbox(label="Conteúdo do e-mail", placeholder="Digite o conteúdo do e-mail", lines=5)
+            arquivo = gr.File(label="Anexar arquivo (.txt ou .pdf)", file_types=[".txt", ".pdf"])
+            btn = gr.Button("Classificar", variant="primary")
+        
+        # Coluna direita: saída formatada
+        with gr.Column(scale=1):
+            saida = gr.Markdown()
+    with gr.Row():
+        gr.Examples(
+                examples=[
+                    ["Pedido de cópia do aditivo contratual de aumento salarial (Ref. 2024).", "Olá RH, Preciso de uma cópia do meu aditivo contratual que formaliza o último aumento salarial (Referência 2024) para fins pessoais. Por favor, envie o documento. Obrigado.", None],
+                    ["Confirmação de Inscrição: Programa de Mentoria 2025.", "Olá, Sua inscrição no Programa de Mentoria 2025 foi confirmada com sucesso.", None],
+                    ["Pedido de cotação para serviço de manutenção de ar-condicionado (Anual).", "Olá Compras, Precisamos de uma cotação para o serviço de manutenção anual preventiva e corretiva de todos os aparelhos de ar-condicionado do escritório. Por favor, solicite 3 propostas. Obrigado.", None],
+                    ["Agradecimento pelo elogio ao Atendimento.", "Olá Fulano, Recebemos seu feedback positivo sobre o atendimento do nosso suporte. Repassamos o elogio à equipe.", None],
+                ],
+                inputs=[assunto, conteudo, arquivo]
+            )
+
+    # Configuração do botão (fica bloqueado durante execução + spinner de progresso)
+    btn.click(
+        fn=predict_email, 
+        inputs=[assunto, conteudo, arquivo], 
+        outputs=saida, 
+        api_name="predict", 
+        show_progress="full"
+    )
+
+# Inicializa aplicação
+demo.launch(share=True)
